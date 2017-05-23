ce85f0f88eefaab646e21c408b98482e731fcc1d
Amit Kushwaha <amit_kushwaha@outlook.com>
2017-05-23 08:14:42 +0530

    runner added for two sets of training data

--- runner/run.py
+++ runner/run.py
@@ -3,9 +3,6 @@ import logging
 from prediction import Prediction
 from train import TrainModel
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(message)s')
-
 SMALL_DATASET = 'small_dataset'
 LARGE_DATASET = 'large_dataset'
 
@@ -18,9 +15,12 @@ def train(dataset):
 def prediction(dataset):
     prediction_model = Prediction(dataset)
     prediction_model.predict()
-    # prediction_model.write_result_to_file()
+    prediction_model.write_result_to_file()
 
 
 if __name__ == '__main__':
-    train(SMALL_DATASET)
-    prediction(SMALL_DATASET)
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(levelname)s %(message)s')
+    for dataset in [SMALL_DATASET, LARGE_DATASET]:
+        train(dataset=dataset)
+        prediction(dataset=dataset)
